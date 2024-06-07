c1008a10f7796ca0f5afb5d74f3ee66db63506ec
Kieran Jeffrey-Smart <kjeffreysmart@pivotal.io>
2024-06-07 20:32:10 +0100

    complete luck test, add save and load, start battle command

--- gameloop.py
+++ gameloop.py
@@ -1,7 +1,10 @@
 # Example file showing a basic pygame "game loop"
 import pygame
 import math
+import random
 import xml.etree.ElementTree as ET
+import json
+
 
 # pygame setup
 pygame.init()
@@ -36,6 +39,8 @@ QUIT_GAME = "QUIT_GAME"
 NEXT_PAGE = "NEXT_PAGE"
 BACK_PAGE = "BACK_PAGE"
 GOTO_PAGE = "GOTO_PAGE"
+TEST_LUCK = "TEST_LUCK"
+FIGHT_MONSTERS = "FIGHT_MONSTERS"
 
 
 intropage = ("0",
@@ -135,31 +140,87 @@ def printPage(page):
             charpos = charpos + 400
             colNum = colNum + 1
 
-def getCarets(pageNode):
-    return [(child.tag, child.text, child.attrib) for child in pageNode.find("story")]
+def appendStory(page, carets):
+    for caret in carets:
+        page[1].append(caret)
+
+def setOptions(page, options):    
+    page[2].clear()
+    for option in options: 
+        page[2].append(option)
+
+def appendTextToStory(page, text):
+    appendStory(page, [("text", text, {})])
+
+def getResult(resultNode):
+    carets = [(child.tag, child.text, child.attrib) for child in resultNode.find("story")]
+    options = [(child.get("key"),  getOptionLabel(child), child.get("command"), getOptionArguments(child)) for child in resultNode if child.tag == "option"]
+
+    return (carets, options)
+
 
 def getOptionArguments(optionNode):
     args = {}
 
     if optionNode.get("command") == "GOTO_PAGE":
-        args["page"] = optionNode.find("page")
+        args["page"] = optionNode.get("page")
+        
+    if optionNode.get("command") == "TEST_LUCK":
+        args["pass"] = getResult(optionNode.find("pass"))
+        args["fail"] = getResult(optionNode.find("fail"))
     
     return args
 
-def getOptions(pageNode):
-    return [(child.get("key"), child.text, child.get("command"), getOptionArguments(child)) for child in pageNode.iter("option")]
-        
+def getOptionLabel(optionNode):
+    if optionNode.find("label") is not None:
+        return optionNode.find("label").text
+    
+    return ""
+    
 def getPage(pageNode):
-    return (pageNode.get("idx"), getCarets(pageNode), getOptions(pageNode))
+    carets = [(child.tag, child.text, child.attrib) for child in pageNode.find("story")]
+    options = [(child.get("key"), getOptionLabel(child), child.get("command"), getOptionArguments(child)) for child in pageNode if child.tag == "option"]
+    
+    return (pageNode.get("idx"), carets, options)
 
 def loadBook(name):
     tree = ET.parse("./books/" + name + ".xml")
     root = tree.getroot()
-    return [getPage(child) for child in root]
+    return ([getPage(child) for child in root if child.tag == "intropage"], [getPage(child) for child in root if child.tag == "gamepage"])
 
+book = loadBook(DEFAULT_BOOK)
+intropages = book[0]
+gamepages = book[1]
+
+currentGameState = INTRO_STATE
 currentPage = 0
-pages = loadBook(DEFAULT_BOOK)
-currentState = INTRO_STATE
+
+luckscore = 6
+
+def getCurrentState():
+    return {"state": currentGameState, "page": currentPage, "luck": luckscore }
+
+def getSaveState():
+    with open('savegame.json', 'r') as save_file:
+        return json.load(save_file)
+    
+def resetGameState(state):
+    book = loadBook(DEFAULT_BOOK)
+    intropages = book[0]
+    gamepages = book[1]
+    currentGameState = INTRO_STATE
+    currentPage = 1
+    luckscore = 6
+
+def setSaveState(state):
+    with open('savegame.json', 'w') as save_file:
+        save_file.write(json.dumps(state))
+
+def testLuck(page):
+    diceCheck = random.randint(1, 6) + random.randint(1, 6)
+    appendTextToStory(page, "Luck: " + str(luckscore) + "\n")
+    appendTextToStory(page, "Roll: " + str(diceCheck) + "\n")
+    return diceCheck <= luckscore
 
 while running:
     # poll for events
@@ -168,31 +229,53 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_F1:
-                loadBook(DEFAULT_BOOK)
-
             if event.key == pygame.K_F12:
                 running = False
                 break
+            
+            if event.key == pygame.K_F11:
+                state = getCurrentState()
+                setSaveState(state)
+                break
 
-            options = intropage[2]
+            if event.key == pygame.K_F10:
+                state = getSaveState()
+                if "state" in state: currentGameState = state["state"]
+                if "page" in state: currentPage = state["page"]
+                if "luck" in state: luckscore = int(state["luck"])
+                break
 
-            if (currentState == GAME_STATE):
-                options = pages[currentPage-1][2]
+            if (currentPage == 0):
+                options = intropage[2]
+            elif (currentGameState == INTRO_STATE):
+                options = intropages[currentPage-1][2]
+            elif (currentGameState == GAME_STATE):
+                options = gamepages[currentPage-1][2]
         
             for option in options:
                 if event.key == pygame.key.key_code(option[0]):   
                     command = option[2]                
                     if command == START_GAME:
-                        currentState = GAME_STATE
+                        book = loadBook(DEFAULT_BOOK)
+                        intropages = book[0]
+                        gamepages = book[1]
+                        currentGameState = INTRO_STATE
                         currentPage = 1
+                        luckscore = 6
+                        break
 
                     if command == QUIT_GAME:
                         running = False
                         break
 
                     if command == NEXT_PAGE:
-                        if (currentPage != len(pages)-1):
+                        if (currentGameState == INTRO_STATE):
+                            if currentPage < len(intropages):
+                                currentPage = currentPage+1
+                            else:
+                                currentPage = 1
+                                currentGameState = GAME_STATE
+                        elif (currentGameState == GAME_STATE and currentPage < len(gamepages)):
                             currentPage = currentPage+1
 
                     if command == BACK_PAGE:
@@ -200,10 +283,26 @@ while running:
                             currentPage = currentPage-1
 
                     if command == GOTO_PAGE:
-                        if ("page" in command[3]):
-                            currentPage = int(command[3]["page"])
+                        if ("page" in option[3]):
+                            currentPage = int(option[3]["page"])
+                            
+                    if command == TEST_LUCK:
+                        if testLuck(gamepages[currentPage-1]):
+                            appendStory(gamepages[currentPage-1], option[3]["pass"][0])
+                            setOptions(gamepages[currentPage-1], option[3]["pass"][1])
+                        else:
+                            appendStory(gamepages[currentPage-1], option[3]["fail"][0])
+                            setOptions(gamepages[currentPage-1], option[3]["fail"][1])  
 
-                    waitForOption = False
+                    if command == FIGHT_MONSTERS:
+                        if resolveBattle(battleState):
+                            appendStory(gamepages[currentPage-1], option[3]["win"][0])
+                            setOptions(gamepages[currentPage-1], option[3]["win"][1])
+                        else:
+                            appendStory(gamepages[currentPage-1], option[3]["defeat"][0])
+                            setOptions(gamepages[currentPage-1], option[3]["defeat"][1])  
+
+                    break
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
@@ -212,15 +311,16 @@ while running:
     pygame.font.init() # you have to call this at the start, 
                    # if you want to use this module.
 
-    if currentState == INTRO_STATE:
+    if currentPage == 0:
         printPage(intropage)
     else:
-        printPage(pages[currentPage-1])
+        if currentGameState == INTRO_STATE:
+            printPage(intropages[currentPage-1])
+        else:
+            printPage(gamepages[currentPage-1])
 
     # flip() the display to put your work on screen
     pygame.display.flip()
-
-    waitForOption = True
 
 
 pygame.quit()
